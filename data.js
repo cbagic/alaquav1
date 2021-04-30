fa54dcb603145ee54d94035df332453244466102
cbagic <blankess@nwfsc.edu>
2021-04-30 09:10:52 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,736 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-welcome-to-alaqua",
+      "name": "Welcome to Alaqua",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7583824400024923,
+          "pitch": 0.06381903643335463,
+          "rotation": 0,
+          "target": "1-bagged-shell-reefs"
+        },
+        {
+          "yaw": -0.2332250670582532,
+          "pitch": 0.0687397631122515,
+          "rotation": 0,
+          "target": "6-standing-on-a-reef"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.4705625040487575e-9,
+          "pitch": -0.21180812403554938,
+          "title": "Welcome to Alaqua<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4619005042930322,
+          "pitch": -0.511763741515038,
+          "title": "Sounds of Alaqua<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.26189901504017854,
+          "pitch": 0.06534681923421459,
+          "title": "Welcome Video Placeholder<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.177331111459976,
+          "pitch": 0.23294606173970855,
+          "title": "Stormwater Runoff<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.536753692152157,
+          "pitch": 0.2739248159271206,
+          "title": "Eastern Lubber Grasshoppers<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.7771131391429558,
+          "pitch": -0.21250457980051607,
+          "title": "Range road<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-bagged-shell-reefs",
+      "name": "Bagged Shell Reefs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.2379548508259113,
+        "pitch": 0.15212085870691183,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1681985798976013,
+          "pitch": 0.11795311226077132,
+          "rotation": 0,
+          "target": "2-creek-mouth"
+        },
+        {
+          "yaw": -1.914978926472548,
+          "pitch": 0.12522992024453217,
+          "rotation": 0,
+          "target": "0-welcome-to-alaqua"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2379548508259113,
+          "pitch": 0.15212085870691183,
+          "title": "A living shoreline is...<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.28376867654788995,
+          "pitch": 0.37302938854900525,
+          "title": "Buried?!",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7402019482811149,
+          "pitch": 0.3331324490396348,
+          "title": "Recycled shell<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.3435353655998163,
+          "pitch": 0.17777198071624767,
+          "title": "Phragmites",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4563652548599322,
+          "pitch": -0.3770214578981541,
+          "title": "Live oak<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-creek-mouth",
+      "name": "Creek Mouth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2816,
+      "initialViewParameters": {
+        "yaw": 2.2680748737193266,
+        "pitch": -0.0025339193132509763,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4183619943081016,
+          "pitch": 0.12798979436829327,
+          "rotation": 0,
+          "target": "1-bagged-shell-reefs"
+        },
+        {
+          "yaw": 2.102154394105245,
+          "pitch": 0.13473550828262404,
+          "rotation": 0,
+          "target": "3-inside-the-creek"
+        },
+        {
+          "yaw": -2.147149356759936,
+          "pitch": 0.026586975450660688,
+          "rotation": 0,
+          "target": "4-planting-on-the-beach"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.315141497046741,
+          "pitch": 0.22306924179181742,
+          "title": "Freshwater Creek<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.488790206597165,
+          "pitch": -0.29117675295082357,
+          "title": "Our Watershed<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9483448372158474,
+          "pitch": -0.10422054263287706,
+          "title": "AmeriCorps members<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.047775872802019,
+          "pitch": -0.18549558831552915,
+          "title": "Tree Cavity<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-inside-the-creek",
+      "name": "Inside the Creek",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2816,
+      "initialViewParameters": {
+        "yaw": -1.257349440722443,
+        "pitch": -0.047677235105787474,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.112306969297558,
+          "pitch": 0.06074883424238209,
+          "rotation": 0,
+          "target": "2-creek-mouth"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6600975312425703,
+          "pitch": 0.11453666443309984,
+          "title": "Cypress Knees<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.545283043480664,
+          "pitch": -0.10047759342193352,
+          "title": "<div>Alligator Habitat</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.0420479790687374,
+          "pitch": 0.3837817388783922,
+          "title": "Tannins",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.1044265897954606,
+          "pitch": -0.17473059992859064,
+          "title": "Palmettos",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.8405027321975362,
+          "pitch": 0.3147816990616086,
+          "title": "Marine Debris<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-planting-on-the-beach",
+      "name": "Planting on the Beach",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.3320963547779954,
+        "pitch": -0.06669586122294824,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4307460269169425,
+          "pitch": 0.05081141362077091,
+          "rotation": 0,
+          "target": "2-creek-mouth"
+        },
+        {
+          "yaw": -2.99584433777607,
+          "pitch": -0.017470996324679078,
+          "rotation": 0,
+          "target": "5-alaquas-salt-marsh"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2734440139228518,
+          "pitch": -0.1779994629894066,
+          "title": "Planting closeup<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.6697369284232373,
+          "pitch": 0.12477771048595798,
+          "title": "Burlap Bags<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.14383439878318427,
+          "pitch": -0.009522584952131652,
+          "title": "<div>PVC pipe</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.2207811577429224,
+          "pitch": -0.28918288565721006,
+          "title": "Charcoal tree<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.4243578313280523,
+          "pitch": 0.5837482150609432,
+          "title": "Rhizome",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.0950752281733322,
+          "pitch": -0.32426431185411175,
+          "title": "Why Plant?<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.0609672902164817,
+          "pitch": -0.05010818158282504,
+          "title": "Spartina patens<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.58014679654322,
+          "pitch": 0.1308555943529548,
+          "title": "Handprints",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-alaquas-salt-marsh",
+      "name": "Alaqua's Salt Marsh",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.086494681699291,
+        "pitch": -0.0949281723861084,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8637602592239659,
+          "pitch": 0.13448156804453504,
+          "rotation": 0,
+          "target": "4-planting-on-the-beach"
+        },
+        {
+          "yaw": -1.6320272695944276,
+          "pitch": 0.12948811978859354,
+          "rotation": 0,
+          "target": "0-welcome-to-alaqua"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9643099681177567,
+          "pitch": 0.030833025581156193,
+          "title": "Edge of Alaqua<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.42696646073856215,
+          "pitch": -0.08919979416979373,
+          "title": "Wearing Waders<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.03289346835211404,
+          "pitch": 0.19692434563946293,
+          "title": "Black Needlerush<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.0418407596309613,
+          "pitch": 0.09867515175440289,
+          "title": "Soft stemmed bulrush<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.070086127636527,
+          "pitch": -0.519454447479557,
+          "title": "Long leaf pine<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.313152337909843,
+          "pitch": -0.05876899993406148,
+          "title": "Salt marsh succession<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.7052528190512923,
+          "pitch": -0.16729313183753902,
+          "title": "Visit cessna<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-standing-on-a-reef",
+      "name": "Standing on a Reef",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.1288537142851762,
+        "pitch": 0.5192655231828951,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.085289451187146,
+          "pitch": 0.20216383066256327,
+          "rotation": 0,
+          "target": "0-welcome-to-alaqua"
+        },
+        {
+          "yaw": -1.6310969451044581,
+          "pitch": 0.20480072906228486,
+          "rotation": 0,
+          "target": "7-reefs-fight-erosion"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.815700491482577,
+          "pitch": 0.5878362370783119,
+          "title": "underwater cordgrass<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.6174315341842238,
+          "pitch": -0.6107263709797195,
+          "title": "Spanish moss<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.1723043644657167,
+          "pitch": 0.2986023355985665,
+          "title": "Reef length + distance from shore<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.9051044700547486,
+          "pitch": 0.17662140784627312,
+          "title": "Habitat",
+          "text": "Bird photo<br>"
+        },
+        {
+          "yaw": 1.1158022481602288,
+          "pitch": 0.2080082807714163,
+          "title": "<div>Intertidal zone</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9164338865361685,
+          "pitch": 0.526605089883315,
+          "title": "Brackish water - excretes salt<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-reefs-fight-erosion",
+      "name": "Reefs Fight Erosion",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2816,
+      "initialViewParameters": {
+        "yaw": -2.6724940315865844,
+        "pitch": -0.06922090609061371,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8325990733489483,
+          "pitch": -0.03562095624830519,
+          "rotation": 0,
+          "target": "6-standing-on-a-reef"
+        },
+        {
+          "yaw": -2.830370721299655,
+          "pitch": 0.051723487147770086,
+          "rotation": 0,
+          "target": "8-gateway-to-the-bay"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7705922850362477,
+          "pitch": -0.2141498229907004,
+          "title": "Erosion is bad. Why?<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.28240241559104184,
+          "pitch": 0.18889029881609432,
+          "title": "Reefs fight erosion, protect trees<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.7713467652651662,
+          "pitch": -0.5972670804513953,
+          "title": "Hardwood forest<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.249904318532707,
+          "pitch": 0.05174480684796734,
+          "title": "tree unstable due to erosion<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.2190551956326843,
+          "pitch": 0.3836532548846918,
+          "title": "Why do we build reefs?<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.7310480443291496,
+          "pitch": 0.31930488317423134,
+          "title": "Driftwood barnacles photo<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-gateway-to-the-bay",
+      "name": "Gateway to the Bay",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2816,
+      "initialViewParameters": {
+        "yaw": -2.3027373678756025,
+        "pitch": -0.09143166350635568,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.983431051505896,
+          "pitch": 0.054992625092197045,
+          "rotation": 0,
+          "target": "7-reefs-fight-erosion"
+        },
+        {
+          "yaw": 2.3044259600554122,
+          "pitch": -0.2178987951831317,
+          "rotation": 0,
+          "target": "0-welcome-to-alaqua"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2443451810666275,
+          "pitch": 0.10732609484927735,
+          "title": "Fetch",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.7336821483494296,
+          "pitch": 0.09882628216599798,
+          "title": "Dolphins!",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.6605560343587733,
+          "pitch": -0.3736493183272245,
+          "title": "cba website<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.07224161585608257,
+          "pitch": -0.44881784499248667,
+          "title": "Hardwood forest<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.700772422027903,
+          "pitch": -0.29594285031075707,
+          "title": "Hammock point?<br>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Alaqua Living Shoreline Virtual Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
